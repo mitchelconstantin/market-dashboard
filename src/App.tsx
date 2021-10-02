495f2b24740544ba3d3a065a586457e4d3517274
Mitchel Constantin <mitchconstantin@gmail.com>
2021-10-02 12:20:52 -0400

    clean up coffee code

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-const callFetchThings = async (cb: any) => {
-  const { data } = await axios.get(`./.netlify/functions/fetchThings`);
-  cb(data);
-};
+const fetchCofee = axios.get(`./.netlify/functions/fetchThings`);
+
 function App() {
-  const [state, setState] = useState();
+  const [coffee, setCoffee] = useState(null);
   useEffect(() => {
-    callFetchThings(setState);
+    fetchCofee.then(({ data }) => setCoffee(data));
   }, []);
-  console.log("state", state);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={state || logo} className="App-logo" alt="logo" />
+        <img src={coffee || logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
