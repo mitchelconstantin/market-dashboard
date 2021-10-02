f5f6bc60402ed67ad19a4d59daf44ac92552246a
Mitchel Constantin <mitchconstantin@gmail.com>
2021-10-02 12:12:50 -0400

    fetch random image with netlify functions

--- netlify/functions/fetchThings/fetchThings.ts
+++ netlify/functions/fetchThings/fetchThings.ts
@@ -1,12 +1,20 @@
-import { Handler } from '@netlify/functions'
-
+import { Handler } from "@netlify/functions";
+const axios = require("axios");
+const API_ENDPOINT = "https://coffee.alexflipnote.dev/random.json";
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
+  try {
+    const { data } = await axios.get(API_ENDPOINT);
+    const body = data.file;
+    console.log("res", data.file);
+    return {
+      statusCode: 200,
+      body,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed fetching data" }),
+    };
   }
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
+const callFetchThings = async (cb: any) => {
+  const { data } = await axios.get(`./.netlify/functions/fetchThings`);
+  cb(data);
+};
 function App() {
+  const [state, setState] = useState();
+  useEffect(() => {
+    callFetchThings(setState);
+  }, []);
+  console.log("state", state);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={state || logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
